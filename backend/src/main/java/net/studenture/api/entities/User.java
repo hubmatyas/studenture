068cbbec90b38e2f1936d3343436b672af908ddf
Jan Vomlel <jv@2scale.cz>
2022-11-07 23:09:54 +0100

    studenture-1 wip

---
+++ backend/src/main/java/net/studenture/api/entities/User.java
@@ -0,0 +1,23 @@
+package net.studenture.api.entities;
+
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", unique = true, nullable = false)
+    private String userName;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public User() {
+    }
+}
