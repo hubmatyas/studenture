8610e2b456d3174a335aea64231bad28b593f824
Matyáš Hub <hubmatyas@gmail.com>
2023-01-14 16:51:17 +0100

    Logo onclick go to homepage

--- frontend/src/components/layout/MainAppBar.js
+++ frontend/src/components/layout/MainAppBar.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { ReactComponent as Logo } from "./logo.svg";
+import { Link } from 'react-router-dom';
 
 export default function MainAppBar() {
   return (
@@ -14,7 +15,9 @@ export default function MainAppBar() {
         <Container maxWidth="xl">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Logo />
+              <Link to="/">
+                <Logo />
+              </Link>
             </Typography>
             <Button color="secondary" variant="contained" href="/">Logout</Button>
           </Toolbar>
